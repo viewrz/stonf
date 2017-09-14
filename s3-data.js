70a8a9f32774bc5cc706eebc551b7d73c7dbfd4a
guillaumervls <guillaumervls@users.noreply.github.com>
2017-09-14 18:13:47 +0200

    fixes

--- s3-data.js
+++ s3-data.js
@@ -4,30 +4,28 @@ import S3 from 'aws-sdk/clients/s3';
 
 const s3 = new S3();
 
-const data = s3
-  .getObject({
-    Key: 'data',
-    Bucket: process.env.S3_BUCKET_DATA_NAME,
-  })
-  .promise()
-  .then(d => JSON.parse(d.Body));
+const data = async () =>
+  s3
+    .getObject({
+      Key: 'data',
+      Bucket: process.env.S3_BUCKET_DATA_NAME,
+    })
+    .promise()
+    .then(d => JSON.parse(d.Body));
 
-const save = async () =>
+const save = async ({ users, confs }) =>
   s3
     .putObject({
-      Body: JSON.stringify(await data),
+      Body: JSON.stringify({ users, confs }),
       Key: 'data',
       ServerSideEncryption: 'AES256',
       Bucket: process.env.S3_BUCKET_DATA_NAME,
     })
     .promise();
 
-const attachSave = (o) => {
-  o.save = save; // eslint-disable-line no-param-reassign
-  return o;
+export default async () => {
+  const d = await data();
+  d.users.save = save.bind(null, d);
+  d.confs.save = save.bind(null, d);
+  return d;
 };
-
-export default async () => ({
-  users: attachSave((await data).users),
-  confs: attachSave((await data).confs),
-});
